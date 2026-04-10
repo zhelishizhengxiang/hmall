f8d5d0a119956f3c247b6ebae991d8a1556d4bfb
zhelishizhengxiang <2114369430@qq.com>
2026-04-11 00:08:30 +0800

    完善.gitignore，清理编译文件、环境配置和IDE设置

---
+++ hmall/hm-geteway/src/main/java/com/hmall/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.hmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version 1.0
+ * @ProjectName: hmall
+ * @Package: com.hmall
+ * @Description:
+ * @Author: Simon
+ * @CreateDate: 2026/4/8
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
